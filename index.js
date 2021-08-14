7e2e96acf2afc9739bf0a918c2e8fba1761fc979
minusInfinite <ashley@minusinfinite.net>
2021-08-14 21:50:31 +0930

    inital Prompt design and testing

--- index.js
+++ index.js
@@ -1,17 +1,178 @@
-const Employee = require("./lib/Employee")
+//node modules
+const inquirer = require("inquirer")
+const chalk = require("chalk")
+
+//class modules
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
 
-const employee = new Employee("james", 1, "james@lopper.com")
-const manager = new Manager("james", 1, "james@lopper.com", "08 9898 7878")
-const engineer = new Engineer("james", 1, "james@lopper.com", "macaw")
-const intern = new Intern("james", 1, "james@lopper.com", "Adelaide Uni")
+/** @type {inquirer.QuestionCollection} */
+const empTypePrompts = {
+    type: "list",
+    name: "empType",
+    message: "Add another employee to the team?",
+    choices: ["Engineer", "Intern", "Finsh"],
+}
 
-console.log(`
-Object      | Values
-employee    | ${employee.name}    | ${employee.id}    | ${employee.email}     |
-manager     | ${manager.name}     | ${manager.id}     | ${manager.email}      | ${manager.officeNumber}
-engineer    | ${engineer.name}    | ${engineer.id}    | ${engineer.email}     | ${engineer.github}
-intern      | ${intern.name}      | ${intern.id}      | ${intern.email}       | ${intern.school}
-`)
+/** @type {inquirer.QuestionCollection} */
+
+const employees = []
+
+function init() {
+    console.info(
+        `💻 ${chalk.bold.underline(
+            "This tool will assist in creating a Team page. Enter the Manager first."
+        )}`
+    )
+    addManager()
+}
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Manager's Name",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Manager's Email",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Manager's Office Number",
+            },
+        ])
+        .then((answers) => {
+            const newEmp = new Manager(
+                answers.name,
+                1,
+                answers.email,
+                answers.officeNumber
+            )
+            employees.push(newEmp.exportHtml())
+            console.info(`✔️ ${chalk.green("Manager added")}`)
+        })
+        .then(() => {
+            inquirer.prompt(empTypePrompts).then((nextAns) => {
+                if (nextAns.empType === "Engineer") {
+                    addEngineer()
+                }
+                if (nextAns.empType === "Intern") {
+                    addIntern()
+                }
+                if (nextAns.empType === "Finsh") {
+                    genHTML()
+                }
+            })
+        })
+        .catch((err) => console.error(err))
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Engineer's Name",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Engineer's email",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Engineer's Github Username",
+            },
+        ])
+        .then((answers) => {
+            const newEmp = new Engineer(
+                answers.name,
+                employees.length + 1,
+                answers.email,
+                answers.github
+            )
+            employees.push(newEmp.exportHtml())
+            console.info(
+                `✔️ ${chalk.green("Engineer added")} Currently ${
+                    employees.length
+                } employees Added`
+            )
+        })
+        .then(() => {
+            inquirer.prompt(empTypePrompts).then((nextAns) => {
+                if (nextAns.empType === "Engineer") {
+                    addEngineer()
+                }
+                if (nextAns.empType === "Intern") {
+                    addIntern()
+                }
+                if (nextAns.empType === "Finsh") {
+                    genHTML()
+                }
+            })
+        })
+        .catch((err) => console.error(err))
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Intern's Name",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Intern's email",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Intern's School",
+            },
+        ])
+        .then((answers) => {
+            const newEmp = new Intern(
+                answers.name,
+                1,
+                answers.email,
+                answers.school
+            )
+            employees.push(newEmp.exportHtml())
+            console.info(
+                `✔️ ${chalk.green("Intern added")} Currently ${
+                    employees.length
+                } Added`
+            )
+        })
+        .then(() => {
+            inquirer.prompt(empTypePrompts).then((nextAns) => {
+                if (nextAns.empType === "Engineer") {
+                    addEngineer()
+                }
+                if (nextAns.empType === "Intern") {
+                    addIntern()
+                }
+                if (nextAns.empType === "Finsh") {
+                    genHTML()
+                }
+            })
+        })
+        .catch((err) => console.error(err))
+}
+
+function genHTML() {
+    console.log(JSON.stringify(employees))
+}
+
+init()
